42641685d3424e142e23db869d0ae764ce25fd58
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-11-20 03:15:42 +0100

    specify the ping request method into the interface tests

--- tests/interfaces/test_ping.py
+++ tests/interfaces/test_ping.py
@@ -1,11 +1,11 @@
 '''
-Test for ping route
+Test for GET ping route
 '''
 import requests
 
 BASE_URL = 'http://localhost'
 
 
-def test_ping_returns_200():
+def test_get_ping_returns_200():
     response = requests.get(BASE_URL + '/ping')
     assert response.status_code == 200
